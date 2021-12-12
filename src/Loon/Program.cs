e928f0b0e71d2e76a1df6fb6a59f39a7faf5f756
Mike Ward <mike@mike-ward.net>
2021-12-12 09:32:04 -0600

    add method to conform to previewer requirements

--- src/Loon/Program.cs
+++ src/Loon/Program.cs
@@ -2,18 +2,25 @@
 
 namespace Loon
 {
-    internal static class Program
+    public static class Program
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         public static void Main(string[] args)
         {
-            AppBuilder
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        // BuildAvaloniaApp() method require for Rider Previewer to work.
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            return AppBuilder
                 .Configure<App>()
                 .UsePlatformDetect()
-                .LogToTrace()
-                .StartWithClassicDesktopLifetime(args);
+                .LogToTrace();
         }
     }
 }
