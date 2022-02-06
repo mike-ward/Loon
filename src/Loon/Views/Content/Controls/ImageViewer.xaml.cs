29f31cc595bf8260251d15a8e146b7e55db3338f
mike-ward <mike@mike-ward.net>
2022-02-06 12:31:34 -0600

    first try at zooming images

--- src/Loon/Views/Content/Controls/ImageViewer.xaml.cs
+++ src/Loon/Views/Content/Controls/ImageViewer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
@@ -26,6 +27,40 @@ namespace Loon.Views.Content.Controls
         {
             Close();
             Source = null;
+        }
+
+        // ReSharper disable once UnusedParameter.Local
+        private void OnPointerWheelChanged(object? sender, PointerWheelEventArgs e)
+        {
+            Resize(e.Delta.Y > 0);
+        }
+
+        private void Resize(bool delta)
+        {
+            var deltaWidth  = Width * 0.05;
+            var deltaHeight = Height * 0.05;
+            var deltaX      = (int)Math.Round(deltaWidth / 2);
+            var deltaY      = (int)Math.Round(deltaHeight / 2);
+
+            var w = delta
+                ? +deltaWidth
+                : -deltaWidth;
+
+            var h = delta
+                ? +deltaHeight
+                : -deltaHeight;
+
+            var px = delta
+                ? -deltaX
+                : +deltaX;
+
+            var py = delta
+                ? -deltaY
+                : +deltaY;
+
+            Position =  new PixelPoint(Position.X + px, Position.Y + py);
+            Width    += w;
+            Height   += h;
         }
     }
 }
