81676fc1d8eff3960e6c3e8e9a6d24705479d9ed
mike-ward <mike@mike-ward.net>
2022-06-25 09:30:47 -0500

    fix short link issue

--- src/Loon/Services/FlowContentService.cs
+++ src/Loon/Services/FlowContentService.cs
@@ -2,14 +2,7 @@
 using System.Data;
 using System.Linq;
 using System.Threading;
-using System.Windows.Input;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Data;
-using Avalonia.Layout;
-using Loon.Converters;
-using Loon.Extensions;
-using Loon.Interfaces;
 using Loon.Models;
 using Twitter.Models;
 
@@ -35,10 +28,10 @@ namespace Loon.Services
 
                 var control = nodeType switch
                 {
-                    FlowContentNodeType.Text    => Run(text),
-                    FlowContentNodeType.Url     => Url(text),
-                    FlowContentNodeType.Mention => Mention(text),
-                    FlowContentNodeType.HashTag => Hashtag(text),
+                    FlowContentNodeType.Text    => InlinesService.Run(text),
+                    FlowContentNodeType.Url     => InlinesService.Url(text),
+                    FlowContentNodeType.Mention => InlinesService.Mention(text),
+                    FlowContentNodeType.HashTag => InlinesService.Hashtag(text),
                     FlowContentNodeType.Media   => null, // images handled elsewhere
                     _                           => throw new ConstraintException("invalid FlowContentNodeType")
                 };
@@ -121,119 +114,6 @@ namespace Loon.Services
                .Concat(hashTags)
                .Concat(media)
                .OrderBy(o => o.Start);
-        }
-
-        private static TextBlock Run(string text)
-        {
-            var textBlock = new TextBlock();
-            textBlock.Classes.Add("normal");
-            textBlock.Text                = text.HtmlDecode();
-            textBlock.VerticalAlignment   = VerticalAlignment.Top;
-            textBlock.HorizontalAlignment = HorizontalAlignment.Left;
-            return textBlock;
-        }
-
-        private static Control Url(string link)
-        {
-            return LinkControl(
-                link.TruncateWithEllipsis(25),
-                App.Commands.OpenUrl,
-                link,
-                ContextMenu(link));
-        }
-
-        private static Control Mention(string screenName)
-        {
-            return LinkControl(
-                "@" + screenName,
-                App.Commands.SetUserProfileContext,
-                screenName);
-        }
-
-        private static Control Hashtag(string text)
-        {
-            return LinkControl(
-                "#" + text,
-                App.Commands.OpenUrl,
-                $"https://twitter.com/hashtag/{text}");
-        }
-
-        private static Control LinkControl(
-            string       text,
-            ICommand     command,
-            object       commandParameter,
-            ContextMenu? contextMenu = null)
-        {
-            var button = new Button();
-            button.Classes.Add("inline");
-            button.Command             = command;
-            button.CommandParameter    = commandParameter;
-            button.ContextMenu         = contextMenu;
-            button.VerticalAlignment   = VerticalAlignment.Top;
-            button.HorizontalAlignment = HorizontalAlignment.Left;
-
-            var textBlock = new TextBlock();
-            textBlock.Classes.Add("hyperlink");
-            textBlock.Tag = commandParameter; // LongUrlService needs this
-            textBlock.SetValue(ToolTip.TipProperty, commandParameter);
-            textBlock.SetValue(ToolTip.ShowDelayProperty, 400);
-
-            if (command == App.Commands.OpenUrl)
-            {
-                var settings = App.ServiceProvider.GetService<ISettings>();
-                var binding = new Binding
-                {
-                    Source             = settings,
-                    Path               = nameof(settings.ShortLinks),
-                    Mode               = BindingMode.OneWay,
-                    Converter          = new ShortLinkConverter(),
-                    ConverterParameter = text.HtmlDecode()
-                };
-                textBlock.Bind(TextBlock.TextProperty, binding);
-            }
-            else
-            {
-                textBlock.Text = text.HtmlDecode();
-            }
-
-            button.Content = textBlock;
-            return button;
-        }
-
-        private static ContextMenu ContextMenu(string link)
-        {
-            var copyLinkAddress = new MenuItem
-            {
-                Header           = App.GetString("copy-link-address"),
-                Command          = App.Commands.CopyToClipboard,
-                CommandParameter = link
-            };
-
-            var emailLinkAddress = new MenuItem
-            {
-                Header           = App.GetString("email-link"),
-                CommandParameter = link,
-                Icon             = new TextBlock { Classes = new Classes("symbol", "padleft"), Text = App.GetString("MailSymbol") }
-            };
-
-            var shareLinkTwitter = new MenuItem
-            {
-                Header           = App.GetString("share-link-twitter"),
-                CommandParameter = link,
-                Icon             = new TextBlock { Classes = new Classes("symbol1", "padleft"), Text = App.GetString("TwitterSymbol") }
-            };
-
-            return new ContextMenu
-            {
-                HorizontalOffset = 15,
-                VerticalOffset   = 10,
-                Items = new[]
-                {
-                    copyLinkAddress,
-                    emailLinkAddress,
-                    shareLinkTwitter
-                }
-            };
         }
     }
 }
---
+++ src/Loon/Services/InlinesService.cs
@@ -0,0 +1,129 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Layout;
+using Loon.Converters;
+using Loon.Extensions;
+
+namespace Loon.Services
+{
+    public static class InlinesService
+    {
+        private static readonly IValueConverter LinkConverter = new ShortLinkConverter();
+
+        public static TextBlock Run(string text)
+        {
+            var textBlock = new TextBlock();
+            textBlock.Classes.Add("normal");
+            textBlock.Text                = text.HtmlDecode();
+            textBlock.VerticalAlignment   = VerticalAlignment.Top;
+            textBlock.HorizontalAlignment = HorizontalAlignment.Left;
+            return textBlock;
+        }
+
+        public static Control Url(string link)
+        {
+            return LinkControl(
+                link.TruncateWithEllipsis(25),
+                App.Commands.OpenUrl,
+                link,
+                ContextMenu(link));
+        }
+
+        public static Control Mention(string screenName)
+        {
+            return LinkControl(
+                "@" + screenName,
+                App.Commands.SetUserProfileContext,
+                screenName);
+        }
+
+        public static Control Hashtag(string text)
+        {
+            return LinkControl(
+                "#" + text,
+                App.Commands.OpenUrl,
+                $"https://twitter.com/hashtag/{text}");
+        }
+
+        private static Control LinkControl(
+            string       text,
+            ICommand     command,
+            object       commandParameter,
+            ContextMenu? contextMenu = null)
+        {
+            var button = new Button();
+            button.Classes.Add("inline");
+            button.Command             = command;
+            button.CommandParameter    = commandParameter;
+            button.ContextMenu         = contextMenu;
+            button.VerticalAlignment   = VerticalAlignment.Top;
+            button.HorizontalAlignment = HorizontalAlignment.Left;
+
+            var textBlock = new TextBlock();
+            textBlock.Classes.Add("hyperlink");
+            textBlock.Tag = commandParameter; // LongUrlService needs this
+            textBlock.SetValue(ToolTip.TipProperty, commandParameter);
+            textBlock.SetValue(ToolTip.ShowDelayProperty, 400);
+
+            if (command == App.Commands.OpenUrl 
+             && text.StartsWith('#') is false) // don't convert hashtag links
+            {
+                var binding = new Binding
+                {
+                    Source             = App.Settings,
+                    Path               = nameof(App.Settings.ShortLinks),
+                    Mode               = BindingMode.OneWay,
+                    Converter          = LinkConverter,
+                    ConverterParameter = text.HtmlDecode()
+                };
+                textBlock.Bind(TextBlock.TextProperty, binding);
+            }
+            else
+            {
+                textBlock.Text = text.HtmlDecode();
+            }
+
+            button.Content = textBlock;
+            return button;
+        }
+
+        private static ContextMenu ContextMenu(string link)
+        {
+            var copyLinkAddress = new MenuItem
+            {
+                Header           = App.GetString("copy-link-address"),
+                Command          = App.Commands.CopyToClipboard,
+                CommandParameter = link
+            };
+
+            var emailLinkAddress = new MenuItem
+            {
+                Header           = App.GetString("email-link"),
+                CommandParameter = link,
+                Icon             = new TextBlock { Classes = new Classes("symbol", "padleft"), Text = App.GetString("MailSymbol") }
+            };
+
+            var shareLinkTwitter = new MenuItem
+            {
+                Header           = App.GetString("share-link-twitter"),
+                CommandParameter = link,
+                Icon             = new TextBlock { Classes = new Classes("symbol1", "padleft"), Text = App.GetString("TwitterSymbol") }
+            };
+
+            return new ContextMenu
+            {
+                HorizontalOffset = 15,
+                VerticalOffset   = 10,
+                Items = new[]
+                {
+                    copyLinkAddress,
+                    emailLinkAddress,
+                    shareLinkTwitter
+                }
+            };
+        }
+    }
+}
