b287bacad3306cacf9807b232c967f4e138fb36b
mike-ward <mike@mike-ward.net>
2022-07-20 09:42:20 -0500

    change font size slider to updown, expand settings

---
+++ src/Loon/Converters/RoundToConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Loon.Converters
+{
+    public class RoundToConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var digits = parameter is int p
+                ? p
+                : 1;
+
+            return value is double d
+                // ReSharper disable once HeapView.BoxingAllocation
+                ? Math.Round(d, digits)
+                : value;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
