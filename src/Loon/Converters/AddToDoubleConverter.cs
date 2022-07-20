c30bf7f761306c13ee68e691bfa2f078cba28433
mike-ward <mike@mike-ward.net>
2022-07-20 08:58:36 -0500

    add converter for tab header font size

---
+++ src/Loon/Converters/AddToDoubleConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Loon.Converters
+{
+    internal sealed class AddToDoubleConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var operand = parameter is double d
+                ? d
+                : 1;
+
+            if (value is double v)
+            {
+                // ReSharper disable once HeapView.BoxingAllocation
+                return v + operand;
+            }
+
+            return value;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
